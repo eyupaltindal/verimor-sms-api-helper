05d5a64947ba77f2994756831b0f6df5d9178653
eyupaltindal <eyupaltindal@gmail.com>
2020-09-13 23:14:24 +0300

    request limit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-// app.use(express.json());
-app.use(express.json({limit: '50mb'}));
+app.use(express.json());
+// app.use(express.json({limit: '50mb'}));
 
 app.use('/', indexRouter);
 
